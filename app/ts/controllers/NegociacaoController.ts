3f508ed8bbf414ebc3d8db3dbd424c065a39539d
George Mayko <mayko.george@gmail.com>
2018-11-24 14:37:44 -0300

    Utilizando padrão de data com barra para conversão

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -13,7 +13,7 @@ class NegociacaoContoller{
     adiciona(event: Event){
         event.preventDefault();
         const negociacao = new Negociacao(
-            new Date(this._inputData.value.replace(/-/g, ',')),
+            new Date(this._inputData.value.replace(/-/g, '/')),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
