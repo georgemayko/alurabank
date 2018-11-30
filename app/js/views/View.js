9beb9c83adfb73f222820bd70a112b51c5934af7
George Mayko <mayko.george@gmail.com>
2018-11-29 21:40:34 -0300

    Deixando a classe View genérica

--- app/js/views/View.js
+++ app/js/views/View.js
@@ -2,4 +2,10 @@ class View {
     constructor(seletor) {
         this._elemento = document.querySelector(seletor);
     }
+    update(modelo) {
+        this._elemento.innerHTML = this.template(modelo);
+    }
+    template(modelo) {
+        throw new Error('Metodo template da classe view deve ser implementado!');
+    }
 }
