87ed8e4b6eb0cc8bb7035d2954552571acb0e572
George Mayko <mayko.george@gmail.com>
2018-12-02 17:56:15 -0300

    Ajuste no decorator domInject

--- app/js/helpers/decorators/domInject.js
+++ app/js/helpers/decorators/domInject.js
@@ -2,16 +2,16 @@ System.register([], function (exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     function domInject(seletor) {
-        return function (target, key) {
+        return function (target, propertyKey) {
             let elemento;
             const getter = function () {
                 if (!elemento) {
-                    console.log(`Buscando ${seletor} para injetar em ${key}`);
+                    console.log(`Buscando ${seletor} para injetar em ${propertyKey}`);
                     elemento = $(seletor);
                 }
                 return elemento;
             };
-            Object.defineProperty(target, key, {
+            Object.defineProperty(target, propertyKey, {
                 get: getter
             });
         };
--- app/ts/helpers/decorators/domInject.ts
+++ app/ts/helpers/decorators/domInject.ts
@@ -1,16 +1,16 @@
 export function domInject(seletor : string){
 
-    return function(target : any, key : string){
+    return function(target : any, propertyKey : string){
         let elemento : JQuery;
         const getter = function (){
             if(!elemento){
-                console.log(`Buscando ${seletor} para injetar em ${key}`);
+                console.log(`Buscando ${seletor} para injetar em ${propertyKey}`);
                 elemento = $(seletor);
             }
             return elemento;
         };
 
-        Object.defineProperty(target, key, {
+        Object.defineProperty(target, propertyKey, {
             get : getter
         });
     }
